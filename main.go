fd96d5d202d85d4234f4628e47dd99b7a975c916
Adesoji Daniel <sojidaniels@gmail.com>
2024-05-01 20:27:10 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.LoadHTMLFiles("templates/index.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html" ,gin.H{
+			"message": "success",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router.Run(fmt.Sprintf(":%s", port)) // listen and serve on 0.0.0.0:8080
+}
